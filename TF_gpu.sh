857d9e47595b8f617164b0197a06d6831aea26ff
Rajeev Atla <rajeev@rajeevatla.com>
2025-11-16 05:09:48 -0500

    add sample docker container approach

---
+++ TF_gpu.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH --job-name=a1-mini-clip
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --time=12:00:00
+#SBATCH --mem=32G
+#SBATCH --cpus-per-task=8
+#SBATCH --constraint=ampere
+#SBATCH --exclude=gpu[018]
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=${USER}@rutgers.edu
+#SBATCH --output=slurm/slurm_%j.out
+#SBATCH --error=slurm/slurm_%j.err
+
+mkdir -p slurm
+
+module purge
+
+module use /projects/community/modulefiles
+module load singularity
+
+srun singularity exec --nv nvcr.io/nvidia/jax:25.10-py3
